0d42da2c1d5359ef2195baafd225a2e731458207
Juan Carlos Salazar <jsalazar@MacBookPro-jsalazar.local>
2026-01-07 14:22:31 -0600

    ajuste en direccion a db y prueba 01

--- regionmiddleware/src/server.js
+++ regionmiddleware/src/server.js
@@ -17,6 +17,11 @@ var pool = verb_utils.pool
 var port = config.port || 8080;
 var app = express()
 
+console.log("port: " + config.port)
+console.log("port 2: " + port)
+console.log("db_name: " + config.db.database)
+console.log("db_port: " + config.db.port)
+
 //app.use(express.static('public'));
 
 app.use(compression({filter:shouldCompress, level:zlib.Z_BEST_COMPRESSION}))
